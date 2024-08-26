92b88fb64977837fe08d4d859988c4d3d79c8270
2023-anik <cse2023anik@gmail.com>
2024-08-26 14:30:02 +0600

    chore: Add user input prompts for age and parents' names

--- input.sh
+++ input.sh
@@ -4,3 +4,9 @@ echo "What's ur name?"
 read name
 echo "Hi there $name"
 echo "Welcome to DevDojo"
+
+read -p "How old'r you? " age
+echo "ur age: ${age}"
+
+read -p "What's ur father's and mother's name? " fatherName motherName
+echo -e "Father's name: ${fatherName}\nMother's name: ${motherName}"
